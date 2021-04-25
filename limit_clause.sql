36f4c454e04aa4dddba946960813b78e99e4e6f5
Muhammad Huzaifah <huzaifah2009@gmail.com>
2021-04-26 06:25:31 +0700

    learn for LIMIT clause

---
+++ limit_clause.sql
@@ -0,0 +1,11 @@
+SELECT * FROM products ORDER BY id LIMIT 5;
+
+SELECT * FROM products ORDER BY id LIMIT 3;
+
+SELECT * FROM products ORDER BY id LIMIT 0, 5;
+
+SELECT * FROM products ORDER BY id LIMIT 5, 5;
+
+SELECT * FROM products ORDER BY id LIMIT 10, 5;
+
+SELECT * FROM products ORDER BY id LIMIT 15, 5;
