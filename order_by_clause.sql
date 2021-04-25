7bbe9ab762eda6571ed119b4345867cbc28cdc1d
Muhammad Huzaifah <huzaifah2009@gmail.com>
2021-04-26 06:13:44 +0700

    learn for ORDER BY clause

---
+++ order_by_clause.sql
@@ -0,0 +1,5 @@
+SELECT * FROM products ORDER BY category;
+
+SELECT id, category, name FROM products ORDER BY category;
+
+SELECT id, category, name, price FROM  products ORDER BY category, price DESC ;
