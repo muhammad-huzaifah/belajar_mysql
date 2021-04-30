575eface9c56f4068d65e3c780ee2d7b2a67f9a5
Muhammad Huzaifah <huzaifah2009@gmail.com>
2021-05-01 06:22:12 +0700

    learn for grouping

---
+++ grouping.sql
@@ -0,0 +1,19 @@
+SELECT COUNT(id) AS 'Total Product', category FROM products GROUP BY category;
+
+SHOW TABLES ;
+
+SELECT * FROM products;
+
+SELECT MAX(price) AS 'Harga Termahal', category FROM products GROUP BY category;
+
+SELECT MIN(price) AS 'Harga Termahal', category FROM products GROUP BY category;
+
+SELECT AVG(price) AS 'Harga Termurah', category FROM products GROUP BY category;
+
+SELECT SUM(quantity) AS 'Total Stock', category FROM products GROUP BY category;
+
+SELECT COUNT(id) AS 'Total', category FROM products GROUP BY category HAVING Total > 5;
+
+SELECT COUNT(id) AS 'Total', category FROM products GROUP BY category HAVING Total > 1;
+
+
